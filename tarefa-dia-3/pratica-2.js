28a101ca8380b08a622a0a0afd51efbb777081b6
Tatiane Paes <tatianevilelapaes@gmail.com>
2022-08-28 20:36:42 -0300

    décimo primeiro commit

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,12 @@
+/*exercicio 2
+*/
+
+console.log("Tabuada de 9")
+
+let contador = 1
+let vTabuada = 9
+
+while (contador <= 10) {
+    console.log(vTabuada + ' x ' + contador + ' = ' + vTabuada * contador)
+    contador = contador + 1    
+}
