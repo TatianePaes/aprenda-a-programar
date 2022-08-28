5eeacf45c425f1f839300ab78a35d13f2f30c400
Tatiane Paes <tatianevilelapaes@gmail.com>
2022-08-28 00:13:58 -0300

    quarto commit

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,10 @@
+let nome = "Tatiane Paes"
+let email = "tatianevilelapaes@gmail.com"
+let profissao = "Tecnica em Eletrônica"
+let idade = 38
+let sonho = "OiapoqueEchui"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(sonho)
