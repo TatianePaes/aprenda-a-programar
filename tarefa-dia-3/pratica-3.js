07b2575a491bc49d16320985a765e3afd27584fe
Tatiane Paes <tatianevilelapaes@gmail.com>
2022-08-28 21:23:37 -0300

    décimo segundo commit

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,16 @@
+//Pratica 3
+
+let n = 16
+let contador = 1 
+
+// while (contador <= n) {
+//     if (contador % 2 !== 0){
+//         console.log(contador)
+//         contador = contador + 1
+//     }
+// }
+
+while (contador <= n) {
+    console.log(contador)
+    contador = contador + 2
+}
